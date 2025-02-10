f7b75c0ab19a14d7005af74d74c4e2fb5272e16d
kks940104 <kks940104@gmail.com>
2025-02-10 15:33:01 +0900

    phone number validator 추가 - kks

--- src/main/java/org/anonymous/member/validators/FindValidator.java
+++ src/main/java/org/anonymous/member/validators/FindValidator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.anonymous.global.validators.MobileValidator;
 import org.anonymous.member.controllers.RequestFindPassword;
 import org.anonymous.member.controllers.RequestJoin;
+import org.anonymous.member.repositories.MemberRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -11,7 +12,12 @@ import org.springframework.validation.Validator;
 
 @Lazy
 @Component
+@RequiredArgsConstructor
 public class FindValidator implements Validator, MobileValidator {
+
+
+    private final MemberRepository repository;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestFindPassword.class);
@@ -22,6 +28,8 @@ public class FindValidator implements Validator, MobileValidator {
         if (errors.hasErrors()) return;
 
         RequestFindPassword form = (RequestFindPassword) target;
+
+
 
         String phoneNumber = form.getPhoneNumber();
 
