3a17cd2ee61f8e98b3da8a19f1c9da552ecbf342
kks940104 <kks940104@gmail.com>
2025-02-14 22:10:55 +0900

    member request 수정

--- src/main/java/org/anonymous/member/controllers/RequestStatus.java
+++ src/main/java/org/anonymous/member/controllers/RequestStatus.java
@@ -3,12 +3,13 @@ package org.anonymous.member.controllers;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.anonymous.member.constants.DomainStatus;
+import org.anonymous.member.entities.Member;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestStatus {
 
-    private String email;
+    private Member member;
 
     private DomainStatus status;
 
